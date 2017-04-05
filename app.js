3c0a9274d82ce867788b104c8585e575af53295a
Luke Hudson <luke@billingsplanroom.com>
2017-04-04 20:38:42 -0600

    add mlab databaase

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://lupacino:7thchamber@ds039411.mlab.com:39411/yelpcamp1");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
